cdd2bca49ca9498f00aa8681e5c1f4ee3741b6e4
Peter Maguire <peter.maguire@ably.com>
2022-10-28 12:56:42 +0100

    spruce up the demo a little bit

--- video-chat-example/src/App.js
+++ video-chat-example/src/App.js
@@ -6,8 +6,8 @@ import { createRef } from "react";
 class App extends React.Component {
 
   state = {
-    draftUsername: "",
     contacts: [],
+    inCall: false,
   }
   client;
 
@@ -17,14 +17,12 @@ class App extends React.Component {
   createClient() {
     console.log("Component mount, making client");
     if(this.client)return console.log("Ignoring reload");
-    this.client = new Client({key: "RUhpyA.eNjWvw:O5J_HASit3mcAyY0w6-QODsgbVlxmkNfEnMbWKRCql4"}, {username: this.state.username});
 
+    this.client = new Client({key: "RUhpyA.eNjWvw:O5J_HASit3mcAyY0w6-QODsgbVlxmkNfEnMbWKRCql4"}, {username: this.state.username});
     this.updateContacts();
     this.client.addEventListener("onContactAdd", this.updateContacts.bind(this));
     this.client.addEventListener("onContactRemove", this.updateContacts.bind(this));
     this.client.addEventListener("onIncomingCall", ({ call })=>{
-      console.log("Incoming call!");
-      console.log(call);
       if(window.confirm(`Accept call from ${call.remoteContact.ablyClientId}?`)) {
         call.accept();
       }else{
@@ -32,15 +30,29 @@ class App extends React.Component {
       }
     });
     this.client.addEventListener("onCallEstablished", ()=>{
-      console.log("Call connected!");
+      this.setState({inCall: true})
       this.localVideoRef.current.srcObject = this.client.localVideo;
       this.remoteVideoRef.current.srcObject = this.client.remoteVideo;
+    });
+    this.client.addEventListener("onCallEnded", ()=>{
+      this.setState({inCall: false});
+      window.alert("Call Ended");
     })
   }
 
   updateContacts(){
     console.log("Contacts update", this.client.contacts);
     this.setState({contacts: this.client.contacts});
+  }
+
+  hangUp(){
+    this.client.activeCall.hangUp();
+    this.setState({inCall: false});
+  }
+
+  call(c){
+    c.call();
+    this.setState({inCall: true});
   }
 
   render(){
@@ -53,8 +65,12 @@ class App extends React.Component {
     }
     return (
       <div className="App">
-        Contacts:
-        {this.state.contacts.map((c)=><button onClick={()=>c.call()} disabled={c.ablyClientId === this.client.ably.clientId}>{c.username}</button>)}
+        <div id="contactsList">
+          <h1>Contacts:</h1>
+          {this.state.contacts.map((c)=><button className="contact" onClick={()=>this.call(c)} disabled={this.state.inCall || c.ablyClientId === this.state.username}>📞 {c.username}</button>)}
+          <hr/>
+          <button onClick={this.hangUp} disabled={!this.state.inCall}>Hang Up</button>
+        </div>
         <video autoPlay muted ref={this.localVideoRef}/>
         <video autoPlay ref={this.remoteVideoRef}/>
       </div>
@@ -64,10 +80,10 @@ class App extends React.Component {
 
 function UsernameInput({onSubmit}) {
   const [username, setUsername] = React.useState("");
-  return <>
+  return <div id="login">
     <input type="text" placeholder="Enter a username" onChange={(e)=>setUsername(e.target.value)} value={username}></input>
     <button onClick={()=>onSubmit(username)}>Log In</button>
-  </>
+  </div>
 }
 
 export default App;
